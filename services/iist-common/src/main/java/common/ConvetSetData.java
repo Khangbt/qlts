3b1666316254691bb344e15bd62415df80a3a64c
Khangbt <buithekhang246@gmail.com>
2020-11-11 16:15:53 +0700

    get

--- services/iist-common/src/main/java/common/ConvetSetData.java
+++ services/iist-common/src/main/java/common/ConvetSetData.java
@@ -1,6 +1,26 @@
 package common;
 
+import java.lang.reflect.Field;
+
 public class ConvetSetData {
-String d="áas";
-  String s="aaa";
+    private static Object xetData(Object entityl, Object dto) {
+        Field[] fields = entityl.getClass().getDeclaredFields();
+        Field[] fieldsDto = dto.getClass().getDeclaredFields();
+        for (Field field : fieldsDto) {
+            for (Field field1 : fields) {
+                if (field1.getName().equals(field.getName()) && (field.getType().equals(field1.getType()))) {
+                    try {
+                        field1.setAccessible(true);
+                        field.setAccessible(true);
+                        field1.set(entityl, field.get(dto));
+
+                    } catch (IllegalAccessException e) {
+                        return null;
+                    }
+
+                }
+            }
+        }
+        return entityl;
+    }
 }
